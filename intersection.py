851234d309f67afda8dc1cc0fbdd69d5337cbdc9
Ivan1353 <97453913+Ivan1353@users.noreply.github.com>
2023-02-03 15:37:23 +0200

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,7 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersection = []
+        for i in nums1:
+            if i in nums2:
+                intersection.append(i)
+        return intersection
