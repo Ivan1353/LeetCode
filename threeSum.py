199252c387b2f1c1c9e82c5704c8383f5a5fa1ea
Ivan1353 <97453913+Ivan1353@users.noreply.github.com>
2023-02-11 13:43:02 +0200

    Add files via upload

---
+++ threeSum.py
@@ -0,0 +1,10 @@
+import itertools
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        sums = []
+        for comb in itertools.combinations(nums, 3):
+            if sum(comb) == 0 and list(sorted(comb)) not in sums:
+                sums.append(list(sorted(comb)))
+        return sums
+
